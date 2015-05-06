f637319ce3526e625084ebb50cffaa972c7ff623
twopisoft <twopisoft@gmail.com>
2015-05-06 20:13:45 +0800

    Constants

---
+++ essence/python/consts.py
@@ -0,0 +1,47 @@
+# Number of slots (periods) per day
+SLOTS = 8
+
+# Number of days per week
+DAYS = 5
+
+# Total slots (periods) per week
+WK_SLOTS = SLOTS * DAYS
+
+# Number of study hours per week per group 
+WEEK_HRS = 16
+
+# Minimum teaching hours per week limit per teacher
+MIN_HRS = 18
+
+# Maximum teaching hours per week limit per teacher
+MAX_HRS = 26
+
+# Minimum teaching hours per day limit per teacher
+MIN_HRSD = 2
+
+# Maximum teaching hours per day limit per teacher
+MAX_HRSD = 8
+
+# Data start row in input xlsx
+START_ROW = 80
+
+# Data end row in input xlsx
+END_ROW = 165
+
+# Day start column id in input xlsx file
+DAY_START_COL = 'K'
+
+# Day end column id in input xlsx file
+DAY_END_COL = 'O'
+
+# Group column id in input xlsx file
+GROUP_COL = 'E'
+
+# Course code column id in input xlsx file
+COURSE_CODE_COL = 'A'
+
+# Course name column id in input xlsx file
+COURSE_NAME_COL = 'B'
+
+# Weekdays names
+WEEKDAYS = ['Sunday','Monday','Tuesday','Wednesday','Thursday']
